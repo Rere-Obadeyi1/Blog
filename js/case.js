f41f864adc1f48762234afece7a867943062138b
Rere-Obadeyi1 <rereloluwaobadeyi7@gmail.com>
2022-07-31 12:00:59 +0100

    Initial commit

---
+++ js/case.js
@@ -0,0 +1,29 @@
+const inputs = document.querySelectorAll(".input-field");
+const toggle_btn =document.querySelectorAll(".toggle");
+const main = document.querySelector("main");
+const bullets = document.querySelectorAll(".bullets span");
+const images = document.querySelectorAll(".image");
+
+inputs.forEach((inp) => {
+    inp.addEventListener("focus", () => {
+        inp.classList.add("active");
+    });
+    inp.addEventListener("blur", () => {
+        if(inp.value !="") return;
+        inp.classList.remove("active");
+    });
+});
+
+toggle_btn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        main.classList.toggle("sign-up-mode");
+    });
+});
+
+function moveSlider(){
+    bullets.forEach(bull => bull.classList.remove("active"));
+    this.classList.add("active");
+}
+bullets.forEach((bullet) => {
+    bullet.addEventListener("click", moveSlider);
+});
